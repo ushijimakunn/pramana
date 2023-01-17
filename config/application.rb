e8498c933777499f79a8f57c2e7eba0c9911eaa2
ushijima eisaku <eisakuushijima@gmail.com>
2023-01-17 22:11:11 +0900

    ADD : Rspec環境の追加

--- config/application.rb
+++ config/application.rb
@@ -30,6 +30,7 @@ module ZENToLiveAsWeAre
     # in config/environments, which are processed later.
     #
     # config.time_zone = "Central Time (US & Canada)"
+    config.time_zone = "Tokyo"
     # config.eager_load_paths << Rails.root.join("extras")
 
     # Don't generate system test files.
