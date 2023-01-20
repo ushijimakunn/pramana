1e1a29ec80c136b48a08c6b7c43a7a14a818059c
ushijima eisaku <eisakuushijima@gmail.com>
2023-01-20 20:59:06 +0900

    adopt tailwindcss #23

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  mode: 'jit',
+  purge: [
+    './app/views/**/*.html.erb',
+    './app/helpers/**/*.rb',
+    './app/javascript/**/*.js',
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
