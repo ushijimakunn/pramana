84a79d497bc9f9b5a1423199b6ae9dacce372d1c
ushijima eisaku <eisakuushijima@gmail.com>
2023-01-22 16:25:06 +0900

    ユーザー新規登録機能、ログインログアウト機能の作成 #14

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
+  get 'users/new'
+  get 'users/create'
   root to: 'static_pages#top'
-  get '/trial_start', to: 'mindfuls#trial_start'
-  get '/trial_end', to: 'mindfuls#trial_end'
+  get 'trial_start', to: 'mindfuls#trial_start'
+  get 'trial_end', to: 'mindfuls#trial_end'
+
+  get 'login', to: 'user_sessions#new'
+  post 'login', to: 'user_sessions#create'
+  delete 'logout', to: 'user_sessions#destroy'
+
+  resources :users, only: %i[new create]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_01_19_115921) do
+ActiveRecord::Schema.define(version: 2023_01_22_064025) do
 
   create_table "mindfulness_effects", force: :cascade do |t|
     t.string "name"
@@ -36,10 +36,13 @@ ActiveRecord::Schema.define(version: 2023_01_19_115921) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name", null: false
     t.string "email", null: false
+    t.string "crypted_password"
+    t.string "salt"
+    t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "mindfulness_exercises", "users"
