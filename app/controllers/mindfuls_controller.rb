dd8a44f8e31872e578114737a66ed79c9bfbcd47
ushijima eisaku <eisakuushijima@gmail.com>
2023-01-29 13:34:55 +0900

    瞑想実績ページの編集、計算ロジックをUserモデルへ移植 #19

--- app/controllers/mindfuls_controller.rb
+++ app/controllers/mindfuls_controller.rb
@@ -1,8 +1,6 @@
 class MindfulsController < ApplicationController
   before_action :login_required
   before_action :get_all_mindfuls, only: %i[index show]
-  before_action :cal_total_time, only: %i[show]
-  before_action :cal_consecutive_days, only: %i[show]
 
   def index; end
   
@@ -27,33 +25,6 @@ class MindfulsController < ApplicationController
 
   def get_all_mindfuls
     @mindfuls = current_user.mindfuls.all
-  end
-
-  def cal_total_time
-    get_all_mindfuls
-    @total_time = 0
-    @mindfuls.each do |mindful|
-      @total_time += mindful.time
-    end
-  end
-
-  #  今日を起点に、連続何日瞑想を行なっているかを計算
-  def cal_consecutive_days
-    get_all_mindfuls
-    @consecutive_days = 0
-    today = Date.today
-    n = 0
-    # レコードを回し、連続実施日数を計算
-    @mindfuls.reverse_each do |mindful|
-      if mindful.date == (today-n)
-        @consecutive_days += 1
-        n += 1
-      elsif mindful.date == (today-n+1) #前回瞑想時と同じ日の時
-        next
-      else 
-        break
-      end
-    end
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,48 @@ class User < ApplicationRecord
 
   validates :email, uniqueness: true, presence: true
   validates :name, presence: true, length: { maximum: 255 }
+
+  # 瞑想時間の合計を計算（秒）
+  def cal_total_time
+    mindfuls = self.mindfuls.all
+    total_time = 0
+    mindfuls.each do |mindful|
+      total_time += mindful.time
+    end
+    return total_time
+  end
+
+  #  連続何日瞑想を行なっているかを計算
+  def cal_consecutive_days
+    mindfuls = self.mindfuls.all
+    consecutive_days = 0
+    today = Date.today
+    n = 0
+    # レコードを回し、連続実施日数を計算
+    mindfuls.reverse_each do |mindful|
+      if mindful.date == (today-n)
+        consecutive_days += 1
+        n += 1
+      elsif mindful.date == (today-n+1) #前回瞑想時と同じ日の時
+        next
+      else 
+        break
+      end
+    end
+    return consecutive_days
+  end
+
+  # 合計日数を計算
+  def cal_total_date
+    mindfuls = self.mindfuls.all
+    array_date = []
+    mindfuls.each do |mindful|
+      array_date.push(mindful.date)
+    end
+    total_date = array_date.uniq.size
+    return total_date
+  end
+  
+  
 end
+
