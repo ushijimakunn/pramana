2359f7d9344087a8d22b3506b675b8b9949a6e3b
ushijima eisaku <eisakuushijima@gmail.com>
2023-02-04 20:01:04 +0900

    ログイン後のページのデザイン修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,12 @@ class User < ApplicationRecord
   has_many :mindfuls
   has_many :mindfulness_types, through: :mindfuls
 
+  validates :name, presence: true, length: { maximum: 255 }
+  validates :email, uniqueness: true, presence: true
   validates :password, length: { minimum: 3 }, if: -> { new_record? || changes[:crypted_password] }
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
 
-  validates :email, uniqueness: true, presence: true
-  validates :name, presence: true, length: { maximum: 255 }
 
   #  連続何日瞑想を行なっているかを計算
   def cal_consecutive_days
